ce04e04f722b086f1de64bc95be096eee0cc65ea
waterbang <409071692@qq.com>
2022-04-01 16:57:34 +0800

    feat(:sparkles:): 1. index.ts只能作为入口文件 2.抽象工厂函数

--- packages/migrate/src/migrate.ts
+++ packages/migrate/src/migrate.ts
@@ -1,35 +1,38 @@
 import path from "node:path";
 import fs from "node:fs";
-import { getWorkspaceContext, createWriteStream, createReadStream } from './fileFactory';
+import { getWorkspaceContext, createReadStream, migragteFactory } from "./fileFactory";
 import { getUserCmdInput, getUserCmdConfirm } from "./cli";
-import os from 'os';
+import os from "os";
+import { indexRule, nodeRule, typeDeclareRule, typeDRule } from "./rule";
 
 let workspaceRoot = path.join(process.cwd());
-const opinionFile = path.join(process.cwd(), 'opinionFile.md');
+const tip = "是否要把以上文件记录下来?";
 const typeFiles: string[] = []; // type类型匹配到的文件
 const nodeFiles: string[] = []; // .node.ts 类型匹配到的文件
 const declareFiles: string[] = []; // @types.ts 这种文件，只用来declare，不可以出现import <spe> 这样的语法
+const indexFiles: string[] = []; // index.ts只允许作为入口文件
 
 const beforeInit = async () => {
-  const result = await getUserCmdConfirm(`此工具将会帮助您的代码风格向pkgm靠拢 ${os.EOL} 您现在是否在您的工作目录下？`);
+  const result = await getUserCmdConfirm(
+    `此工具将会帮助您的代码风格向pkgm靠拢 ${os.EOL} 您现在是否在您的工作目录下？`,
+  );
   if (result) {
     return init();
   }
-  const workspaces = await getUserCmdInput('请输入您工作区的位置(相对路径)：');
+  const workspaces = await getUserCmdInput("请输入您工作区的位置(相对路径)：");
   workspaceRoot = path.join(process.cwd(), workspaces);
   init();
-}
+};
 beforeInit();
 
 const init = async () => {
-  const {
-    fileDirs,
-    filesArrs
-  } = await getWorkspaceContext(workspaceRoot)
-  fileDirs.forEach(async (dir, index) => await mainMigrateFactory(filesArrs[index] as string[] | string, dir));
+  const { fileDirs, filesArrs } = await getWorkspaceContext(workspaceRoot);
+  fileDirs.forEach(
+    async (dir, index) => await mainMigrateFactory(filesArrs[index] as string[] | string, dir),
+  );
   await askDeveloperOpinion();
-  console.log('风格标记结束');
-}
+  console.log("风格标记结束");
+};
 
 /**
  * 给用户选择，是否把不符合pkgm的记录下来
@@ -37,95 +40,80 @@ const init = async () => {
 const askDeveloperOpinion = async () => {
   if (typeFiles.length !== 0) {
     console.log(typeFiles);
-    const result = await getUserCmdConfirm(`以上文件不符合pkgm的*.t.ts 风格是否要把以上文件记录下来?`);
+    const result = await getUserCmdConfirm(`${typeDRule} ${tip}`);
     if (result) {
-      await typeMigrateFactory(typeFiles);
+      await migragteFactory(typeFiles)(typeDRule);
     }
   }
   if (nodeFiles.length !== 0) {
-    console.log(nodeFiles)
-    const result = await getUserCmdConfirm(`以上文件不符合pkgm的*#node.ts风格是否要把以上文件记录下来?`);
+    console.log(nodeFiles);
+    const result = await getUserCmdConfirm(`${nodeRule} ${tip}`);
     if (result) {
-      await nodeMigrateFactory(nodeFiles);
+      await migragteFactory(nodeFiles,true)(nodeRule);
     }
   }
   if (declareFiles.length !== 0) {
     console.log(declareFiles);
-    const result = await getUserCmdConfirm(`pkgm规定@types.ts 这种文件，只用来declare，不可以出现import <spe> 是否要把以上文件记录下来?`);
+    const result = await getUserCmdConfirm(`${typeDeclareRule} ${tip}`);
     if (result) {
-      await declareMigrateFactory(declareFiles);
+      await migragteFactory(declareFiles,true)(typeDeclareRule);
     }
   }
-}
+  if (indexFiles.length !== 0) {
+    console.log(indexFiles);
+    const result = await getUserCmdConfirm(`${indexRule} ${tip}`);
+    if (result) {
+      await migragteFactory(indexFiles,true)(indexRule);
+    }
+  }
+};
 
 /**
  * 收集匹配规则的文件
- * @param {FileArray | File} files 
- * @param {string} dir 
- * @returns 
+ * @param {FileArray | File} files
+ * @param {string} dir
+ * @returns
  */
-const mainMigrateFactory = async (files:Array<string> | string, dir:string) => {
+const mainMigrateFactory = async (files: Array<string> | string, dir: string) => {
   if (!Array.isArray(files)) {
     const stat = fs.lstatSync(files);
     const is_direc = stat.isDirectory();
     // 如果是文件，直接判断规则
     if (!is_direc) {
-      fileFilterFactory(files)
+      fileFilterFactory(files);
     }
     return;
   }
   files.map(async (fileName, index) => {
     const filesDir = path.join(dir, fileName);
     fileFilterFactory(filesDir);
-  })
+  });
 };
 
 /**
- * 把记录的文件写入
- * @param {FileArray} typeFiles 
- */
-const typeMigrateFactory = async (typeFiles:Array<string>) => {
-  createWriteStream(opinionFile, typeFiles, '以下文件不符合pkgm的*.t.ts 风格');
-}
-/**
- * 把记录的文件写入
- * @param {FileArray} nodeFiles 
- */
-const nodeMigrateFactory = (nodeFiles:Array<string>) => {
-  createWriteStream(opinionFile, nodeFiles, '以下文件不符合pkgm的*#node.ts风格', {
-    'flags': 'a'
-  });
-}
-/**
- * 把记录的文件写入
- * @param {FileArray} declareFiles 
- */
-const declareMigrateFactory = (declareFiles:Array<string>) => {
-  createWriteStream(opinionFile, declareFiles, '@types.ts 这种文件，只用来declare，不可以出现import <spe>', {
-    'flags': 'a'
-  });
-}
-
-/**
  * 处理文件规则
- * @param {string} filesDir 
+ * @param {string} filesDir
  */
-const fileFilterFactory = async (filesDir:string) => {
+const fileFilterFactory = async (filesDir: string) => {
   const fileName = path.basename(filesDir);
   if (/@type[s]?\.[t,j,m,c]s[x]?$/.test(fileName)) {
     typeFiles.push(filesDir);
-    await declareFilesRule(filesDir)
+    await declareFilesRule(filesDir);
   }
 
   if (/\.[^d]+\.[t,j,m,c]s[x]?$/.test(fileName)) {
     nodeFiles.push(filesDir);
   }
-}
+
+  if (/index.ts/.test(fileName)) {
+    await indexFilesRule(filesDir);
+  }
+};
 
 /**
  * 处理@types.ts里有import的
- * @param {string} filesDir 
- * @returns 
+ * @param {string} filesDir
+ * @returns
  */
 const declareFilesRule = (filesDir: string) => {
   return new Promise<void>(async (resolve) => {
@@ -133,6 +121,16 @@ const declareFilesRule = (filesDir: string) => {
     if (/import\(.+\)/g.test(dataChunk as string)) {
       declareFiles.push(filesDir);
     }
-    resolve()
-  })
-}
+    resolve();
+  });
+};
+
+const indexFilesRule = (filesDir: string) => {
+  return new Promise<void>(async (resolve) => {
+    const dataChunk = await createReadStream(filesDir);
+    if (/(function)/g.test(dataChunk as string)) {
+      indexFiles.push(filesDir);
+    }
+    resolve();
+  });
+};
