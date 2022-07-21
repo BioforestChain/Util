90521fd5f51fd0004d8c99f7fd4a63cc7c2d6da4
kingsword09 <kingsword09@gmail.com>
2022-07-21 15:28:42 +0800

    :bug: 修复decorator缺少tslib问题

--- packages/decorator/#bfsp.ts
+++ packages/decorator/#bfsp.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "@bfchain/pkgm-bfsp";
 import { genBfspConfig } from "../../base.bfsp";
 export default defineConfig((info) => {
   const config: Bfsp.UserConfig = {
-    ...genBfspConfig("@bfchain/util-decorator", info),
+    ...genBfspConfig("@bfchain/util-decorator", info, {
+      dependencies: {
+        tslib: "^2.4.0",
+      },
+    }),
     deps: [
       "@bfchain/util-typings",
       // "@bfchain/util-extends-promise-out"
